ff1127cc2c054ccbf1f0a6ba345f42fb4b72499b
vanditvyas <vandit.vyas@gmail.com>
2026-06-07 20:16:48 +0530

    Refactor scroll event and enhance header shadow

--- script.js
+++ script.js
@@ -1,11 +1,62 @@
-window.addEventListener(“scroll”, () => {
+// Header shadow
 
-const header = document.querySelector(”.header”);
+window.addEventListener("scroll", () => {
 
-if(window.scrollY > 50){
-header.style.boxShadow = “0 5px 20px rgba(0,0,0,.08)”;
-}else{
-header.style.boxShadow = “none”;
-}
+    const header = document.querySelector(".header");
+
+    if(window.scrollY > 50){
+        header.style.boxShadow =
+        "0 10px 30px rgba(0,0,0,.08)";
+    } else {
+        header.style.boxShadow = "none";
+    }
+
+});
+
+
+// Image Modal
+
+const modal =
+document.getElementById("imageModal");
+
+const modalImg =
+document.getElementById("modalImage");
+
+const images =
+document.querySelectorAll(".gallery-image");
+
+const closeBtn =
+document.querySelector(".close-modal");
+
+
+images.forEach(img => {
+
+    img.addEventListener("click", () => {
+
+        modal.style.display = "flex";
+
+        modalImg.src = img.src;
+
+        modalImg.alt = img.alt;
+
+    });
+
+});
+
+
+closeBtn.addEventListener("click", () => {
+
+    modal.style.display = "none";
+
+});
+
+
+modal.addEventListener("click", (e) => {
+
+    if(e.target === modal){
+
+        modal.style.display = "none";
+
+    }
 
 });
